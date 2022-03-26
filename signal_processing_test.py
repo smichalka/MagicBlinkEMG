f905b91e0f7180375dce275e11fc75769c541849
achakraborty <achakraborty@olin.edu>
2022-03-25 21:01:55 -0400

    working notch filter and fft analysis on basic signal

--- signal_processing_test.py
+++ signal_processing_test.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.fft import fft, fftfreq
+import mne.filter as filter
 
 pi = np.pi
 A1 = 1
@@ -17,6 +18,14 @@ y=A1*np.sin(2*pi*f1*t)+A2*np.sin(2*pi*f2*t)
 yf = fft(y)
 xf = fftfreq(int(1/.0001), .0001)
 
-plt.plot(xf, np.abs(yf))
+#plt.plot(xf, np.abs(yf))
 plt.xlim([-60, 60])
+#plt.show()
+
+filtered_data = filter.notch_filter(y, 1/.0001, 50)
+yf = fft(filtered_data)
+xf = fftfreq(int(1/.0001), .0001)
+plt.plot(xf, yf)
+#plt.plot(t, filtered_data)
+#plt.xlim([0, 1])
 plt.show()
