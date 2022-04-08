7eb9f44e4e4dacc17acddfdcb0aebee9173990a8
achakraborty <achakraborty@olin.edu>
2022-04-07 23:55:15 -0400

    cleaned up repo

---
+++ markerTests/CMarkersTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using LSL;
+
+namespace LSLExamples
+{
+    static class ReceiveStringMarkers
+    {
+        public static void Main(string[] args)
+        {
+            // wait until an EEG stream shows up
+            StreamInfo[] results = LSL.LSL.resolve_stream("name", "MyMarkerStream");
+
+            // open an inlet and print meta-data
+            using StreamInlet inlet = new StreamInlet(results[0]);
+            results.DisposeArray();
+            Console.Write(inlet.info().as_xml());
+
+            // read samples
+            string[] sample = new string[1];
+            while (!Console.KeyAvailable)
+            {
+                inlet.pull_sample(sample);
+                Console.WriteLine(sample[0]);
+            }
+        }
+    }
+}
--- signal_processing_test.py
+++ signal_processing_test.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.fft import fft, fftfreq
+from scipy.fft import rfft, rfftfreq
 import scipy.signal as filter
 
 pi = np.pi
@@ -13,22 +13,24 @@ t = np.arange(0, 1, .0001)
 y=A1*np.sin(2*pi*f1*t)+A2*np.sin(2*pi*f2*t)
 
 plt.plot(t, y)
+plt.title("Original Signal)")
 plt.show()
 
-yf = fft(y)
-xf = fftfreq(int(1/.0001), .0001)
-
+yf = rfft(y)
+xf = rfftfreq(int(1/.0001), .0001)
 plt.plot(xf, np.abs(yf))
 plt.xlim([-60, 60])
+plt.title("First FFT")
 plt.show()
 
 a, b = filter.iirnotch(50, 50/200, int(1/.0001))
 print(a, b)
-filtered_data = filter.lfilter(a, b, y)
-yf = fft(filtered_data)
-xf = fftfreq(int(1/.0001), .0001)
-print(type(y[0]))
-#plt.plot(xf, yf)
+filtered_data = filter.filtfilt(a, b, y)
+yf = rfft(filtered_data)
+xf = rfftfreq(int(1/.0001), .0001)
+plt.plot(xf, yf)
+plt.show()
+
 plt.plot(t, filtered_data)
 plt.xlim([0, 1])
 plt.show()
